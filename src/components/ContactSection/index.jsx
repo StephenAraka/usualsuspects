395ade9df8d8a2eda7bcfd4517ec04d0cb710ab4
StephenAraka <stevenaraka@gmail.com>
2020-08-03 09:37:07 +0300

    Add URLs

--- src/components/ContactSection/index.jsx
+++ src/components/ContactSection/index.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './ContactSection.css';
 import { ReactComponent as Instagram } from '../../assets/images/instagram.svg';
 import { ReactComponent as Facebook } from '../../assets/images/facebook.svg';
+import { Link } from 'react-router-dom';
 
 const ContactSection = () => {
   return (
     <div className="ContactSection">
-      <Instagram className="ContactIcon" />
-      <Facebook className="ContactIcon" />
+      <a href="https://www.instagram.com/theusualsuspectsug" target="_blank" rel="noopener noreferrer"><Instagram className="ContactIcon" /></a>
+      <a href="https://www.facebook.com/theusualsuspectsug" target="_blank" rel="noopener noreferrer"><Facebook className="ContactIcon" /></a>
     </div>
   )
 }
